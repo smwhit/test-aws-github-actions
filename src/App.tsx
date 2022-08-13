b6f3aab5d77735ce1fd296dd6c09c507161e5fb5
Simon <simon.whittemore@gmail.com>
2022-08-13 11:41:02 +0100

    env

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {process.env.REACT_APP_MAIN_TEXT}
         </p>
         <a
           className="App-link"
